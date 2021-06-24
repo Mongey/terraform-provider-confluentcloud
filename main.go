2774f926a9b655403aca5b81518420a2c9bb2d69
Conor Mongey <conor@mongey.net>
2021-06-24 03:36:50 +0100

    Add go:generate cmd to main

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 func main() {
 	plugin.Serve(&plugin.ServeOpts{ProviderFunc: c.Provider})
 }
